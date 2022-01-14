1e186da48137d19604292f170ee6b3e9cfc1c8be
Geoff Sallee <gsallee@paciolan.com>
2022-01-14 12:40:11 -0800

    Initial commit of scaffolded projects

---
+++ taskmaster-service/src/main/java/com/kittensmasher/taskmaster/service/TaskmasterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kittensmasher.taskmaster.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasterServiceApplication.class, args);
+	}
+
+}
